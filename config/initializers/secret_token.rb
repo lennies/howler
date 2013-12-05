518d442159f24c914236955519f6c2589dd2d1dc
lennies <lenniesliwinski@gmail.com>
2013-12-05 12:54:36 -0500

    installed rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Howler::Application.config.secret_key_base = 'be5938a913d5c30bef6ce2cfb9e77ed7a7435ee81483be13aef4b1bd56b3a5ce65311e0e4594d619885cd21e833b8fbf53c0faacd472a10ad56b9f055c354918'
